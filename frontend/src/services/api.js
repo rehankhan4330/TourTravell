d3c48b5eda06ceb4d0c901d57979cdb0170bc381
Rehan Khan <rehankhan4227@gmail.com>
2026-09-14 21:29:56 +0530

    Configure production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,14 +2,16 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
     const token = getToken();
+
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
